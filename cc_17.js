e7422885155a20b166cc0cf3b810183004ab5f20
Jbarb25 <barboza205@usf.edu>
2025-03-30 13:21:07 -0400

    Task 2: Sales rep’s clients and total spent for a specific client
    Created a sales rep class that linked the sales rep to their client list. Use methods to add a client to the sales rep's client list and then be able to find the client name and total amount to return and log for each customer.

--- cc_17.js
+++ cc_17.js
@@ -25,6 +25,39 @@ const customer = new Customer("John", "johnK@example.com");
 customer.addPurchase(50);
 customer.addPurchase(30);
 customer.addPurchase(20);
+const customer2 = new Customer("Abby", "abby3@example.com");
+customer2.addPurchase(40);
 
 //Log the total amount for the customer's purchase history 
 console.log(customer.getTotalAmount())
+console.log(customer2.getTotalAmount())
+
+
+//Task 2: Sales Rep Class
+class SalesRep{
+    constructor(name){
+        this.name = name; //assign sales rep name
+        this.clients = []; //list of sales rep clients
+    }
+
+    //Method to add client/customer to the list
+    addClient(customer){
+        this.clients.push(customer)
+    }
+
+    //Method to find client by name and return total spent
+    getClientTotal(name){   
+        const client = this.clients.find(client => client.name === name);  //find the client name associated with this.namen in customer class
+        if (client){  //if the client is found as a customer
+            return `Client Name: ${client.name}, Client Total: ${client.getTotalAmount()}`} //return the customer name and their total amount from customer class
+        else {
+            return `Client ${name} not found.`} //if not then alert not found
+    }
+}
+
+//Test and log the new sales rep addition and their client with their total
+const salesRep = new SalesRep("Emma")
+salesRep.addClient(customer)
+salesRep.addClient(customer2)
+console.log(salesRep.getClientTotal("John"))
+console.log(salesRep.getClientTotal("Abby"))
